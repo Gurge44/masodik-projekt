124ebc63795486a114cf10382895b0cffa1a2630
Gurge44 <gurge44.minecraft@gmail.com>
2023-11-23 13:12:58 +0100

    just save

--- Program/Program.cs
+++ Program/Program.cs
@@ -90,17 +90,12 @@
 
             while (true)
             {
-                Console.WriteLine("Most kereshetsz a bevitt adatok között!\n  1. Keresés típusra\n  2. Keresés névre\n  3. Keresés paraméterek között\n  4. Keresés árra\n  5. Statisztika készítése a típusokról\n  6. Adott termékkategória összes árának csökkentése adott százalékkal\n  7. Egy alkatrész adatainak módosítása\n  8. Pontos keresés (az összes adat szükséges)");
+                Console.WriteLine("Most kereshetsz a bevitt adatok között!\n  1. Keresés típusra\n  2. Keresés névre\n  3. Keresés paraméterek/adatok között\n  4. Keresés árra\n  5. Statisztika készítése a típusokról\n  6. Adott termékkategória összes árának csökkentése adott százalékkal\n  7. Egy alkatrész adatainak módosítása\n  8. Pontos keresés (az összes adat szükséges)");
 
                 int result = 0;
                 while (result <= 0 || result > 8)
                 {
                     var answer = Console.ReadLine();
-                    if (answer?.ToLower() == "quit")
-                    {
-                        stop = true;
-                        break;
-                    }
                     if (!int.TryParse(answer, out result))
                     {
                         Console.WriteLine("Helytelen bevitel. A választott metódus sorszámát írd be!");
@@ -115,36 +110,165 @@
                         string typeToFind = Console.ReadLine() ?? string.Empty;
                         if (typeToFind == string.Empty) break;
 
-                        Piece[]? matches = Pieces.Where(x => x.Type.Contains(typeToFind)).ToArray();
+                        Piece[]? typeMatches = Pieces.Where(x => x.Type.Contains(typeToFind)).ToArray();
 
-                        if (matches == null || !matches.Any())
+                        if (typeMatches == null || !typeMatches.Any())
                         {
                             Console.ForegroundColor = ConsoleColor.Red;
-                            Console.WriteLine("The type you searched for ({typeToFind}) was not found.");
+                            Console.WriteLine($"A típus, amire rákerestél ({typeToFind}), nem található.");
                             Console.ResetColor();
                             break;
                         }
 
-                        Console.WriteLine("Matches:");
-                        foreach (var piece in matches)
+                        Console.WriteLine("Találatok:");
+                        foreach (var piece in typeMatches)
                         {
-                            Console.WriteLine("Type: " + piece.Type + ", Name: " + piece.Name + ", Data: " + piece.Parameters + ", Cost: " + piece.Cost + "Ft");
+                            Console.WriteLine("Típus: " + piece.Type + ", Név: " + piece.Name + ", Adatok: " + piece.Parameters + ", Ár: " + piece.Cost + " Ft");
                         }
 
                         break;
-                    case 2:
+
+                    case 2: // Keresés névre
+
+                        Console.Write("Keresendő név: ");
+                        string nameToFind = Console.ReadLine() ?? string.Empty;
+                        if (nameToFind == string.Empty) break;
+
+                        Piece[]? nameMatches = Pieces.Where(x => x.Name.Contains(nameToFind)).ToArray();
+
+                        if (nameMatches == null || !nameMatches.Any())
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine($"A név, amire rákerestél ({nameToFind}), nem található.");
+                            Console.ResetColor();
+                            break;
+                        }
+
+                        Console.WriteLine("Találatok:");
+                        foreach (var piece in nameMatches)
+                        {
+                            Console.WriteLine("Típus: " + piece.Type + ", Név: " + piece.Name + ", Adatok: " + piece.Parameters + ", Ár: " + piece.Cost + " Ft");
+                        }
+
                         break;
-                    case 3:
+
+                    case 3: // Keresés paraméterek között
+
+                        Console.Write("Keresendő paraméter/adat: ");
+                        string parameterToFind = Console.ReadLine() ?? string.Empty;
+                        if (parameterToFind == string.Empty) break;
+
+                        Piece[]? parameterMatches = Pieces.Where(x => x.Name.Contains(parameterToFind)).ToArray();
+
+                        if (parameterMatches == null || !parameterMatches.Any())
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine($"Az adat, amire rákerestél ({parameterToFind}), nem található.");
+                            Console.ResetColor();
+                            break;
+                        }
+
+                        Console.WriteLine("Találatok:");
+                        foreach (var piece in parameterMatches)
+                        {
+                            Console.WriteLine("Típus: " + piece.Type + ", Név: " + piece.Name + ", Adatok: " + piece.Parameters + ", Ár: " + piece.Cost + " Ft");
+                        }
+
                         break;
-                    case 4:
+
+                    case 4: // Keresés árak között
+
+                        Console.Write("Alsó és felső határ:\n  Formátum: [alsó határ] [felső határ]\nPélda: '6500 13900'\n  ");
+                        int upperLimit = -1;
+                        int lowerLimit = -1;
+                        try
+                        {
+                            var limits = Console.ReadLine()?.Split(' ');
+                            if (limits == null || limits.Length != 2) break;
+
+                            if (!int.TryParse(limits[0], out lowerLimit)) break;
+                            if (!int.TryParse(limits[1], out upperLimit)) break;
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Invalid format.");
+                            break;
+                        }
+                        if (upperLimit < 0 || lowerLimit < 0) break;
+
+                        var costMatches = Pieces.Where(x => x.Cost >= lowerLimit && x.Cost <= upperLimit).ToArray();
+
+                        if (costMatches == null || !costMatches.Any())
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine($"Nem található egy alkatrész sem, amelynek az ára a két megadott határ között van ({lowerLimit} - {upperLimit}).");
+                            Console.ResetColor();
+                            break;
+                        }
+
+                        Console.WriteLine("Találatok:");
+                        foreach (var piece in costMatches)
+                        {
+                            Console.WriteLine("Típus: " + piece.Type + ", Név: " + piece.Name + ", Adatok: " + piece.Parameters + ", Ár: " + piece.Cost + " Ft");
+                        }
+
                         break;
-                    case 5:
+
+                    case 5: // Statisztika készítése a típusokról
+
+                        Dictionary<string, List<Piece>> types = new();
+                        foreach (var piece in Pieces)
+                        {
+                            if (types.TryGetValue(piece.Type, out var n))
+                            {
+                                n.Add(piece);
+                            }
+                            else
+                            {
+                                types[piece.Type] = new()
+                                {
+                                    piece
+                                };
+                            }
+                        }
+
+                        foreach (var type in types)
+                        {
+                            Console.WriteLine($"\n\n\"{type.Key}\":\n");
+                            foreach (var piece in type.Value)
+                            {
+                                Console.WriteLine("Típus: " + piece.Type + ", Név: " + piece.Name + ", Adatok: " + piece.Parameters + ", Ár: " + piece.Cost + " Ft");
+                            }
+                        }
+
                         break;
-                    case 6:
+
+                    case 6: // Adott termékkategória összes árának csökkentése adott százalékkal
+
+                        Console.Write("Az árcsökkentett típus: ");
+                        string typeToLower = Console.ReadLine() ?? string.Empty;
+                        Console.Write("Az árak hány százalékkal legyenek csökkentve? ");
+                        string input = Console.ReadLine() ?? string.Empty;
+                        if (!int.TryParse(input, out var lowerWithPercent)) break;
+
+                        var percentage = (int)Math.Round((1 - (lowerWithPercent / 100f)) * 100);
+                        Console.WriteLine($"New percentage: {percentage}%");
+
+                        foreach (var piece in Pieces.Where(x => x.Type == typeToLower).ToArray())
+                        {
+                            Console.Write($"Cost before: {piece.Cost}");
+                            piece.Cost = (int)Math.Round(piece.Cost - (float)(piece.Cost * (percentage / 100f)));
+                            Console.WriteLine($", Cost after: {piece.Cost}");
+                        }
+
                         break;
-                    case 7:
+
+                    case 7: // Egy alkatrész adatainak módosítása
+
                         break;
-                    case 8:
+
+                    case 8: // Pontos keresés (az összes adat szükséges)
+
                         break;
                 }
             }
