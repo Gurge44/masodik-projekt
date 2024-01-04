978cf1bdee8e71f59ce530899daa9549183c6035
Balazs212 <90392911+Balazs212@users.noreply.github.com>
2024-01-04 22:05:42 +0100

    Weblap: Add files via upload (#10)
    A legvéglegesebb.

---
+++ projekt2 konfig weblap - Szabó Balázs/dataconverter.py
@@ -0,0 +1,7 @@
+#Ez a kód adta hozzá a képeket a data.txt-hez.
+import random 
+colors = ["turq","blue","green","yellow","violet","pink","red","orange",]
+with open("data.txt", "r") as file_old, open("newdata.txt", "w") as new_file:
+        for i in file_old:
+            line=i.strip()+';'+colors[random.randint(0,len(colors)-1)]+".png\n"
+            new_file.write(line)
